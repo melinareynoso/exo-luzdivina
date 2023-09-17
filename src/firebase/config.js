69c6a609ef8d4703d6c6bf7d2ddc86e6d4ea8763
Melina <96024732+melinareynoso@users.noreply.github.com>
2023-09-17 18:24:21 -0300

    borro archivos

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD-HRIpL0YZzXI8P9LuHZEXoMA0S6gPRRc",
-  authDomain: "exo-amarres-7875d.firebaseapp.com",
-  projectId: "exo-amarres-7875d",
-  storageBucket: "exo-amarres-7875d.appspot.com",
-  messagingSenderId: "886860695457",
-  appId: "1:886860695457:web:7767d54fc133c101acede0",
-  measurementId: "G-69YKJ11YP4"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD-HRIpL0YZzXI8P9LuHZEXoMA0S6gPRRc",
+//   authDomain: "exo-amarres-7875d.firebaseapp.com",
+//   projectId: "exo-amarres-7875d",
+//   storageBucket: "exo-amarres-7875d.appspot.com",
+//   messagingSenderId: "886860695457",
+//   appId: "1:886860695457:web:7767d54fc133c101acede0",
+//   measurementId: "G-69YKJ11YP4"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// getAnalytics(app);
